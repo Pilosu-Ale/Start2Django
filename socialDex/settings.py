5ae131f4934a27c4d1b91ee1665f1cee85370587
Pilosu-Ale <pilosu.alessandro@gmail.com>
2021-06-08 00:42:52 +0200

    add allowed host

--- socialDex/settings.py
+++ socialDex/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hj=*jai%frg7^$hm^5jo5yuf66v6-gia73(q05@q+0)d$w1c*y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '3.67.124.59']
 
 
 # Application definition
